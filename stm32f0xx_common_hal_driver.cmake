33e68d643602050f45fad0d2c3f0dd334a8f91ca
Giulio Dalla Vecchia <giulio.dallavecchia@eas.it>
2025-01-09 14:57:10 +0100

    chore: add support for cmake

---
+++ stm32f0xx_common_hal_driver.cmake
@@ -0,0 +1,21 @@
+#Description: CMSIS Include For Cortex-M, ARMv8-M, ARMv8.1-M; user_visible: True
+include_guard(GLOBAL)
+message("stm32f0xx_common_hal_driver component is included.")
+
+file(GLOB HAL_SRC_FILES    
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32f0xx_hal.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32f0xx_hal_cortex.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32f0xx_hal_rcc.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32f0xx_hal_rcc_ex.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32f0xx_hal_gpio.c"
+)
+
+target_sources(${PROJECT_NAME} PRIVATE
+	${HAL_SRC_FILES}
+)
+
+target_include_directories(${PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/Inc
+)
+
+
